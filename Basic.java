e833912b8d90af0f77ca6a099fc43f652d729e23
Abhishek Dudhagave <abhishekdudhagave9@gmail.com>
2023-05-19 17:20:50 +0530

    initial commit

---
+++ Basic.java
@@ -0,0 +1,9 @@
+public class Basic{
+    public void greet(){
+        System.out.print("Hello World !");
+    }
+    public static void main(String []args){
+        Basic basic=new Basic();
+        basic.greet();
+    }
+}
